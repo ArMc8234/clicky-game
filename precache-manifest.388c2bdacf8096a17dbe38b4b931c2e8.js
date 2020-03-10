11e2efb4124ddc4a0093a5ed506e2ebec42600ea
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-03-09 23:45:24 -0400

    Updates

---
+++ precache-manifest.388c2bdacf8096a17dbe38b4b931c2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eac399e73ee7aea224867b7bda5d50f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6fc849a8401b5649a3d1",
+    "url": "/clicky-game/static/css/main.a0bff717.chunk.css"
+  },
+  {
+    "revision": "5ad9017b2c2c8e53d446",
+    "url": "/clicky-game/static/js/2.bf4d043c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.bf4d043c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fc849a8401b5649a3d1",
+    "url": "/clicky-game/static/js/main.ecec3ded.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
